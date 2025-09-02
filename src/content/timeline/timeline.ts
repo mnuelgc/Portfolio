e7b9165da44cab58dde83594fff18345312083a9
Manuel Gómez Cámara <manuelgc_12@hotmail.com>
2025-09-02 19:18:12 +0200

    timeline responsive

---
+++ src/content/timeline/timeline.ts
@@ -0,0 +1,72 @@
+import type {Item} from "../../types/items"
+
+export const items: Item[] = [
+    {
+        tag: "DESARROLLO",
+        title: "NTTData",
+        tagColor:"green",
+        subtitle: "Desarrollador de aplicaciones Android",
+        date: "ene 2025 — Actualidad",
+        logo: "./logos/nttIcon.jpeg",
+        description: "Desarrollar aplicacion de Android con Jetpack Compose y Kotlin y mantenimiento de aplicación Legacy con Java/Kotlin y XML ",
+        },
+        {
+        tag: "DOCENCIA",
+        title: "IES La Nucia",
+        subtitle: "Profesor de Informática",
+        date: "sept 2023 — sept 2024",
+        logo: "./logos/gvicon.png",
+        },
+        {
+        tag: "TITULACIÓN",
+        tagColor: "blue",
+        title: "Universidad de Alicante",
+        subtitle:
+            "Máster Universitario en Desarrollo de Software para dispositivos móviles",
+        date: "sept 2023 — jul 2024",
+        logo: "./logos/uaicon.png",
+        },
+        {
+        tag: "DOCENCIA",
+        title: "IES Pere Maria Orts i Bosch",
+        subtitle: "Profesor de Informática",
+        date: "oct 2022 — sept 2023",
+        logo: "./logos/gvicon.png",
+        },
+        {
+        tag: "DESARROLLO",
+        title: "Crisalix", 
+        tagColor:"green",
+        subtitle: "Desarrollador Unity 3D VR",
+        date: "abr 2022 — oct 2022",
+        logo: "./logos/crisalixicon.jpg",
+        description:
+            "Desarrollar aplicacion de Android con Jetpack Compose y Kotlin y mantenimiento de aplicación Legacy con Java/Kotlin y XML ",
+        },
+        {
+        tag: "TITULACIÓN",
+        tagColor: "blue",
+        title: "Universidad de Alicante",
+        subtitle: "Máster Profesorado Secundaria y Formación Profesional",
+        date: "sept 2022 — jul 2023",
+        logo: "./logos/uaicon.png",
+        },
+        {
+        tag: "DESARROLLO",
+        title: "CypeSoft",
+        tagColor:"green",
+        subtitle: "Desarrollador Unity 3D AR/VR",
+        date: "ene 2018 — jul 2021",
+        logo: "./logos/cypeIcon.png",
+        description:
+            "Desarrollar aplicacion de Android con Jetpack Compose y Kotlin y mantenimiento de aplicación Legacy con Java/Kotlin y XML ",
+        },
+        {
+        tag: "TITULACIÓN",
+        tagColor: "blue",
+        title: "Universidad de Alicante",
+        subtitle: "Ingieneria Multimedia",
+        date: "sept 2013 — dec 2017",
+        logo: "./logos/uaicon.png",
+        }
+]
